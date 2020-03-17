c181c0bc58524a05713acbe48ff00f849dd0af35
TsengMJ <tsngmj@gmail.com>
2020-03-18 00:14:48 +0800

    Image Restorer

---
+++ Install/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##
+DataPath="../Data"
+
+## Install required python packages
+#pip install -r requirements.txt
+
+## Download open-images-dataset
+#mkdir -p $DataPath
+#aws s3 --no-sign-request sync s3://open-images-dataset/train $DataPath/Train
+#aws s3 --no-sign-request sync s3://open-images-dataset/validation $DataPath/Validation
+#aws s3 --no-sign-request sync s3://open-images-dataset/test $DataPath/Test
+
